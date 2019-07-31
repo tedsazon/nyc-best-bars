0ae42cc549d904e46db9de5f8bdc126d65dfb06a
Ted Sazon <tedsazon@gmail.com>
2019-07-31 02:34:23 +0000

    first commit

---
+++ lib/nyc/best/bars.rb
@@ -0,0 +1,10 @@
+require "nyc/best/bars/version"
+
+module Nyc
+  module Best
+    module Bars
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/nyc/best/bars/version.rb
@@ -0,0 +1,7 @@
+module Nyc
+  module Best
+    module Bars
+      VERSION = "0.1.0"
+    end
+  end
+end
